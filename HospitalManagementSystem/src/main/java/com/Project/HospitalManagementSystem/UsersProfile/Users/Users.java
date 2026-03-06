585ad268904077344dca78d0b9c2ed1aa92e9e8a
shwetha-s-codes <shwethakodimar29@gmail.com>
2026-03-06 17:57:10 +0530

    User Table Structure Created

--- HospitalManagementSystem/src/main/java/com/Project/HospitalManagementSystem/UsersProfile/Users/Users.java
+++ HospitalManagementSystem/src/main/java/com/Project/HospitalManagementSystem/UsersProfile/Users/Users.java
@@ -1,20 +1,33 @@
 package com.Project.HospitalManagementSystem.UsersProfile.Users;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+
 @Entity
 @Table(name="Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userID;
+    private Long userID;
     @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false,unique = true)
+    private String emailID;
     //A user can have many roles and vice versa
     @ManyToMany(fetch = FetchType.EAGER)
     //Table containing the foreign keys of User & Roles makes sure that there is no redundant entries
